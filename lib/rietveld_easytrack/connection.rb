f5c401e545f58ea2e9eb15371a21c5f60bcd4524
Snowy Leopard <ricklucassen@gmail.com>
2017-03-10 13:34:42 +0100

    Added use of secondary host info when the primary server produces an error

--- lib/rietveld_easytrack/connection.rb
+++ lib/rietveld_easytrack/connection.rb
@@ -5,31 +5,84 @@ require 'uri/open-scp'
 module RietveldEasytrack
   module Connection
 
-    def self.send_file(file, remote_path)
-      Net::SCP.upload!(
-        RietveldEasytrack.configuration.hostname_primary,
-        RietveldEasytrack.configuration.username_primary,
-        StringIO.new(file),
-        remote_path,
-        :ssh => {
-          :password => RietveldEasytrack.configuration.password_primary,
-          :port => RietveldEasytrack.configuration.port_primary
-        }
-      )
-      # end
+    def self.send_file(file, remote_path, secondary = nil)
+      begin
+        Net::SCP.upload!(
+          self.config(secondary)[:hostname],
+          self.config(secondary)[:username],
+          StringIO.new(file),
+          remote_path,
+          :ssh => {
+            :password => self.config(secondary)[:password],
+            :port => self.config(secondary)[:port]
+          }
+        )
+      rescue Net::SSH::AuthenticationFailed
+        return 'Authentication failed'
+      rescue Net::SSH::ConnectionTimeout
+        return 'Connection timeout' if secondary
+        return self.send_file(file, remote_path, true)
+      rescue Exception => e
+        STDERR.puts e
+        return 'Something went wrong' if secondary
+        STDERR.puts 'Something went wrong, trying secondary server'
+        return self.send_file(file, remote_path, true)
+      end
     end
 
-    def self.read_file(path)
-      file = open("scp://#{RietveldEasytrack.configuration.username_primary}@#{RietveldEasytrack.configuration.hostname_primary}#{path}", :ssh => { :password => RietveldEasytrack.configuration.password_primary, :port => RietveldEasytrack.configuration.port_primary }).read
+    def self.read_file(path, secondary = nil)
+      begin
+        file = open("scp://#{self.config(secondary)[:username]}@#{self.config(secondary)[:hostname]}#{path}", :ssh => { :password => self.config(secondary)[:password], :port => self.config(secondary)[:port] }).read
+      rescue Net::SSH::AuthenticationFailed
+        return 'Authentication failed'
+      rescue Net::SSH::ConnectionTimeout
+        return 'Connection timeout' if secondary
+        return self.read_file(path, true)
+      rescue Exception => e
+        STDERR.puts e
+        return 'Something went wrong' if secondary
+        STDERR.puts 'Something went wrong, trying secondary server'
+        return self.read_file(path, true)
+      end
     end
 
     # Returns array of full path file locations in the given dir
-    def self.dir_list(dir, date = nil)
-      date = Time.now().to_date.to_s if date.nil?
-      Net::SSH.start(RietveldEasytrack.configuration.hostname_primary, RietveldEasytrack.configuration.username_primary, :password => RietveldEasytrack.configuration.password_primary, :port => RietveldEasytrack.configuration.port_primary) do |ssh|
-        ssh.exec!("find #{dir} -mindepth 1 -newermt #{date}") do |channel, stream, data|
-          return data.split("\n")
+    def self.dir_list(dir, date = nil, secondary = nil)
+      begin
+        date = Time.now().to_date.to_s if date.nil?
+        Net::SSH.start(self.config(secondary)[:hostname], self.config(secondary)[:username], :password => self.config(secondary)[:password], :port => self.config(secondary)[:port]) do |ssh|
+          ssh.exec!("find #{dir} -mindepth 1 -newermt #{date}") do |channel, stream, data|
+            return data.split("\n")
+          end
         end
+      rescue Net::SSH::AuthenticationFailed
+        return 'Authentication failed'
+      rescue Net::SSH::ConnectionTimeout
+        return 'Connection timeout' if secondary
+        return self.dir_list(dir, date, true)
+      rescue Exception => e
+        STDERR.puts e
+        return 'Something went wrong' if secondary
+        STDERR.puts 'Something went wrong, trying secondary server'
+        return self.dir_list(dir, date, true)
+      end
+    end
+
+    def self.config(secondary = nil)
+      if secondary
+        {
+          hostname: RietveldEasytrack.configuration.hostname_secondary,
+          username: RietveldEasytrack.configuration.username_secondary,
+          password: RietveldEasytrack.configuration.password_secondary,
+          port: RietveldEasytrack.configuration.port_secondary
+        }
+      else
+        {
+          hostname: RietveldEasytrack.configuration.hostname_primary,
+          username: RietveldEasytrack.configuration.username_primary,
+          password: RietveldEasytrack.configuration.password_primary,
+          port: RietveldEasytrack.configuration.port_primary
+        }
       end
     end
   end
