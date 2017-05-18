41293fe230dad0e68b308111e04af572a53d4cd1
Mark Lucassen <mark@marklucassen.nl>
2017-05-18 21:26:46 +0200

    extended questionnare

--- lib/rietveld_easytrack/activity_registration.rb
+++ lib/rietveld_easytrack/activity_registration.rb
@@ -33,7 +33,20 @@ module RietveldEasytrack
       parsed_file[:timestamp] = as.at_xpath('.//start').content if parsed_file[:timestamp].nil?
       parsed_file[:start_time] = as.at_xpath('.//start').content if as.at_xpath('.//start')
       parsed_file[:end_time] = as.at_xpath('.//end').content if as.at_xpath('.//end')
-	  parsed_file[:questionnaireReport] = as.at_xpath('.//questionnaireReport/questionnaireId').content if as.at_xpath('.//questionnaireReport/questionnaireId')
+
+	  q = as.xpath('.//questionnaireReport') if as.xpath('.//questionnaireReport')
+
+	  questionnare = []
+
+	  questionnare[:questionnaireId] = q.at_xpath('.//questionnaireId').content if as.at_xpath('.//questionnaireId')
+	  questionnare[:questionnaireVersion] = q.at_xpath('.//questionnaireVersion').content if as.at_xpath('.//questionnaireVersion')
+	  questionnare[:timestamp] = q.at_xpath('.//timestamp').content if as.at_xpath('.//timestamp')
+	  questionnare[:questionnaireVersion] = q.at_xpath('.//questionnaireVersion').content if as.at_xpath('.//questionnaireVersion')
+
+	  # TODO loop door answer blocks...
+	  # questionnare[:answer] = q.at_xpath('.//answer').content if as.at_xpath('.//answer')
+
+	  parsed_file[:questionnaireReport] = questionnare
 
       # Task references
       task_reference = as.at_xpath('.//taskReference')
