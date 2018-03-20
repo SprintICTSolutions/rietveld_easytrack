9cd84063ec83ae3deaa5bc3a623c62e92a9389a1
Mark Lucassen <mark@agropro.nl>
2018-03-20 11:40:59 +0100

    added possibility to replace task

--- lib/rietveld_easytrack/task_management.rb
+++ lib/rietveld_easytrack/task_management.rb
@@ -3,12 +3,13 @@ require 'fileutils'
 module RietveldEasytrack
   module TaskManagement
 
-    def self.send_task(tasks)
+    def self.send_task(tasks, replace = false)
       raise ArgumentError, 'Data invalid, please check your data' if tasks.empty?
       # Make sure tasks is an array
       tasks = Array(tasks)
 
       template = File.read(File.join(RietveldEasytrack.root, '/lib/rietveld_easytrack/templates/task_management.rb'))
+      template = File.read(File.join(RietveldEasytrack.root, '/lib/rietveld_easytrack/templates/task_management_replace.rb')) if replace
       xml = Nokogiri::XML('<?xml version = "1.0" encoding = "UTF-8" standalone ="no"?>')
 
       xml_tasks = ''
---
+++ lib/rietveld_easytrack/templates/task_management_replace.rb
@@ -0,0 +1,68 @@
+xml.operation('xmlns' => 'http://www.easytrack.nl/integration/taskmanagement/2011/02',
+  'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance') {
+  xml.operationId params[:operation_id]
+  xml.asset {
+    xml.code params[:asset][:code]
+  }
+  xml.update {
+    xml.updateMode 'replace'
+    xml.trips {
+      xml.trip {
+        xml.code params[:trip][:code]
+        xml.name params[:trip][:name]
+        xml.description params[:trip][:description]
+        xml.sequence params[:trip][:sequence]
+        xml.plannedStart params[:trip][:planned_start] if params[:trip][:planned_start]
+        xml.plannedFinish params[:trip][:planned_finish] if params[:trip][:planned_finish]
+        xml.locations {
+          for location in params[:trip][:locations]
+            xml.location{
+              xml.code location[:code]
+              xml.name location[:name]
+              xml.description location[:description]
+              xml.sequence location[:sequence] if location[:sequence]
+              xml.position {
+                if location[:coordinates]
+                  xml.coordinate {
+                    xml.latitude location[:coordinates][:latitude]
+                    xml.longitude location[:coordinates][:longitude]
+                  }
+                end
+                if location[:address]
+                  xml.address {
+                    xml.street location[:address][:street]
+                    xml.number location[:address][:number]
+                    xml.zipcode location[:address][:zipcode]
+                    xml.city location[:address][:city]
+                    xml.country location[:address][:country]
+                  }
+                end
+              }
+              if location[:contact]
+                xml.contacts {
+                  xml.contact {
+                    xml.name location[:contact][:name]
+                    xml.phoneNumber location[:contact][:phone_number]
+                  }
+                }
+              end
+              xml.tasks {
+                for task in location[:tasks]
+                  xml.task {
+                    xml.code task[:code]
+                    xml.name task[:name]
+                    xml.description task[:description]
+                    xml.taskType task[:type]
+                    xml.sequence task[:sequence]
+                    xml.plannedStart task[:planned_start] if task[:planned_start]
+                    xml.plannedFinish task[:planned_finish] if task[:planned_finish]
+                  }
+                end
+              }
+            }
+          end
+        }
+      }
+    }
+  }
+}
